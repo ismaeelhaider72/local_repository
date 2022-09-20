210e5e4e307cfb534ee764826a80da15f3ec190d
ismaeelhaider12 <90700806+ismaeelhaider12@users.noreply.github.com>
2022-09-20 10:28:29 +0500

    Create install_python.sh

---
+++ install_python.sh
@@ -0,0 +1,10 @@
+apt update   
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev -y
+wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+ls
+tar -xf Python-3.7.4.tgz
+cd Python-3.7.4
+./configure --enable-optimizations
+make altinstall
+rm Python-3.7.4.tgz
+python3.7 --version
